4e2a029687c3b1b9b750596cc01307e8da6e8a65
dooxsav <dooxsav@gmail.com>
2023-10-30 17:07:54 +0100

    20231030 5

--- thinkbio_backend/app/routes/GRC_routes.py
+++ thinkbio_backend/app/routes/GRC_routes.py
@@ -15,8 +15,15 @@ def GRC_test_route():
 @GRC_bp.route('/GRC/extraire_information_PDF', methods=['POST'])
 def GRC_extraire_information():
     if request.method == 'POST':
+        # attribution du fichier
+        file = request.files.get('file') # le fichier devra s'appeler file !
+        # Vérification présence d'un fichier
+        if not (file and file.filename and file.filename.endswith('.pdf')):
+            return jsonify({"error": "Fichier incorrect dans la requête"}), 400 # Bad Request
+        
+        
         return toto(), 200
     else:
-        return jsonify({"error": "Méthode non autorisée"}), 405
+        return jsonify({"error": "Méthode non autorisée"}), 405 # Not Allowed
     return toto()
 
