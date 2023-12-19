5fa3901097b416d8adfb67775266b46c22a62b55
dooxsav <dooxsav@gmail.com>
2023-12-19 17:17:37 +0100

    202312191717

--- frontend/src/components/HistoriqueClient/HistoriqueClient.component.jsx
+++ frontend/src/components/HistoriqueClient/HistoriqueClient.component.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
-import "./HistoriqueClient.style.css";
 import { useForm } from "react-hook-form";
 import { apiService } from "../../services/API_think.service";
 
+import "./HistoriqueClient.style.css";
+
 const HistoriqueClient = () => {
-  /** CONSTANTES  */
+  const [error, setError] = useState();
+  const [data, setData] = useState([]);
+  const [nomclient, setnomclient] = useState()
+  const [disabledButton, setDisabledButton] = useState(false);
+
   const {
     register,
     handleSubmit,
-    setValue, // Ajout de setValue depuis useForm
+    setValue,
     formState: { errors },
   } = useForm();
 
-  /** STATES LOCAL */
-  const [error, setError] = useState();
-  const [data, setData] = useState([]);
-  const [disabledButton, setDisabledButton] = useState(false);
-
-  /** Methodes */
   const onSubmit = async (data) => {
     setDisabledButton(true);
     try {
@@ -25,7 +24,7 @@ const HistoriqueClient = () => {
         `/situation/?codeclient=${data.CodeClient}`
       );
       setData(response);
-      console.log("Données récupérées avec succès:", response);
+      setnomclient(response.nom) ; // Récupérer le nom depuis la réponse
     } catch (error) {
       console.log("Erreur lors de la récupération des données:", error.message);
       setError(
@@ -35,63 +34,136 @@ const HistoriqueClient = () => {
     setDisabledButton(false);
   };
 
-  // Regroupement des données par 'numero_document'
-  const groupedData = data.reduce((acc, item) => {
-    if (!acc[item.numero_document]) {
-      acc[item.numero_document] = [];
+  let columns = [];
+  let rows = [];
+
+  if (data.length > 0) {
+    const groupedData = data.reduce((acc, item) => {
+      if (!acc[item.numero_document]) {
+        acc[item.numero_document] = {
+          documents: [],
+          totalMontantHT: 0,
+        };
+      }
+      acc[item.numero_document].documents.push(item);
+      acc[item.numero_document].totalMontantHT += item.montant_HT;
+      return acc;
+    }, {});
+
+    const groupedArray = Object.values(groupedData);
+
+    if (groupedArray.length > 0 && groupedArray[0].documents.length > 0) {
+      const keys = Object.keys(groupedArray[0].documents[0]);
+
+      // Filtrer les colonnes 'code_lot_facture' et 'date_livraison_document' des clés
+      const filteredKeys = keys.filter(
+        (key) =>
+          key !== "code_lot_facture" &&
+          key !== "date_livraison_document" &&
+          key !== "CodeClient" &&
+          key !== "compte_de_vente" &&
+          key !== "id" &&
+          key !== "volume" &&
+          key !== "code_postal" &&
+          key !== "quantite_unit"
+      );
+
+      columns = filteredKeys.map((key) => {
+        return {
+          Header: key,
+          accessor: key,
+        };
+      });
+
+      rows = groupedArray.flatMap((group) =>
+        group.documents.map((doc) => {
+          const { code_lot_facture, date_livraison_document, ...rest } = doc; // Exclure code_lot_facture et date_livraison_document
+          const sanitizedRow = {};
+
+          Object.entries(rest).forEach(([key, value]) => {
+            if (value !== "nan") {
+              sanitizedRow[key] = value;
+            }
+          });
+
+          return sanitizedRow;
+        })
+      );
     }
-    acc[item.numero_document].push(item);
-    return acc;
-  }, {});
+  }
 
-  // Conversion de l'objet regroupé en tableau
-  const groupedArray = Object.values(groupedData);                                                                                          
-
-  console.log(groupedArray);
-
-  // Fonction pour mettre à jour la valeur en majuscules à chaque changement
   const handleInputChange = (e) => {
     const value = e.target.value.toUpperCase();
-    setValue("CodeClient", value); // Met à jour la valeur avec react-hook-form
+    setValue("CodeClient", value);
   };
 
-  /** render */
+
   return (
     <div className="historiqueClient-container container card p-1">
       <div className="card-header">
         <h4>Historique & Récapitulatif Client</h4>
       </div>
+      <div className="card-entete">
+        <div className="card-body">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="form-group row align-items-center"
+          >
+            <label htmlFor="" className="col-form-label">
+              Numéro de Client :
+            </label>
+            <div className="col-sm-3">
+              <input
+                type="text"
+                id="inputField"
+                className="form-control p-1 m-1"
+                maxLength={7}
+                {...register("CodeClient")}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col">
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg"
+                disabled={disabledButton}
+              >
+                Accéder
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="card-client">
+          <div className="card-client-label">
+            <strong>Prénom : </strong> 
+          </div>
+          <div className="card-client-label">
+          <strong>Nom : {nomclient || "vide"} </strong> 
+          </div>
+          
+        </div>
+      </div>
 
       <div className="card-body">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="form-group row align-items-center"
-        >
-          <label htmlFor="" className="col-sm-2 col-form-label text-right">
-            Numéro de Client :
-          </label>
-          <div className="col-sm-3">
-            <input
-              type="text"
-              id="inputField"
-              className="form-control p-1 m-1"
-              maxLength={7}
-              {...register("CodeClient")}
-              onChange={handleInputChange} // Écoute les changements et met à jour en majuscules
-            />
-          </div>
-          <div className="col-sm-3">
-            <button
-              type="submit"
-              className="btn btn-primary btn-lg"
-              disabled={disabledButton}
-            >
-              Accéder
-            </button>
-          </div>
-        </form>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th key={column.accessor}>{column.Header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row, index) => (
+              <tr key={index}>
+                {columns.map((column) => (
+                  <td key={column.accessor}>{row[column.accessor]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <div className="card-body"></div>
     </div>
   );
 };
